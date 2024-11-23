e0dae56538a564c130b382f8e3f53582aab897cd
claustra01 <shinya430.g@gmail.com>
2024-11-24 03:02:55 +0900

    fix: lint

--- app/hooks/useWheelDelta.ts
+++ app/hooks/useWheelDelta.ts
@@ -1,39 +1,37 @@
 import { useEffect, useState } from "hono/jsx";
 
 interface WheelEventState {
-  deltaX: number;
-  deltaY: number;
+	deltaX: number;
+	deltaY: number;
 }
 
 export const useMouseWheel = () => {
-  const [wheelState, setWheelState] = useState<WheelEventState>({
-    deltaX: 0,
-    deltaY: 0,
-  });
+	const [wheelState, setWheelState] = useState<WheelEventState>({
+		deltaX: 0,
+		deltaY: 0,
+	});
 
-  const resetDelta = () => {
-    setWheelState({
-      deltaX: 0,
-      deltaY: 0,
-    });
-  }
+	const resetDelta = () => {
+		setWheelState({
+			deltaX: 0,
+			deltaY: 0,
+		});
+	};
 
-  useEffect(() => {
-    const handleWheel = (event: WheelEvent) => {
-      setWheelState(
-        (prevState) => ({
-          deltaX: prevState.deltaX + Math.abs(event.deltaX),
-          deltaY: prevState.deltaY + Math.abs(event.deltaY),
-        })
-      );
-    };
+	useEffect(() => {
+		const handleWheel = (event: WheelEvent) => {
+			setWheelState((prevState) => ({
+				deltaX: prevState.deltaX + Math.abs(event.deltaX),
+				deltaY: prevState.deltaY + Math.abs(event.deltaY),
+			}));
+		};
 
-    window.addEventListener("wheel", handleWheel);
+		window.addEventListener("wheel", handleWheel);
 
-    return () => {
-      window.removeEventListener("wheel", handleWheel);
-    };
-  }, []);
+		return () => {
+			window.removeEventListener("wheel", handleWheel);
+		};
+	}, []);
 
-  return { wheelState, resetDelta };
+	return { wheelState, resetDelta };
 };
