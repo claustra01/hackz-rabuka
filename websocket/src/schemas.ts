7c3b2c3617ac319d97be72d87d673c7e17360e78
claustra01 <shinya430.g@gmail.com>
2024-11-23 20:23:41 +0900

    add: define type guard function

--- websocket/src/schemas.ts
+++ websocket/src/schemas.ts
@@ -1,73 +1,94 @@
 // client -> server
 export type connectRoomMessage = {
-  type: "connect";
-  roomHash: string;
-  clientId: string;
-  message: "create" | "join";
-}
+	type: "connect";
+	roomHash: string;
+	clientId: string;
+	message: "create" | "join";
+};
 
 // bloadcast
 export type UpdateFireMessage = {
-  type: "updateFire";
-  roomHash: string;
-  clientId: string;
-  value: number;
-}
+	type: "updateFire";
+	roomHash: string;
+	clientId: string;
+	value: number;
+};
 
 // server -> client
 export type SystemMessage = {
-  type: "system";
-  roomHash: string;
-  message: "start" | "finish";
-}
+	type: "system";
+	roomHash: string;
+	message: "start" | "finish";
+};
 
 // server -> client
 export type ResultMessage = {
-  type: "result";
-  roomHash: string;
-  result: Map<string, number>[]; // [{clientId: value}]
-}
+	type: "result";
+	roomHash: string;
+	result: Map<string, number>[]; // [{clientId: value}]
+};
 
-export const isConnectRoomMessage = (msg: any): msg is connectRoomMessage => {
-  return msg.type === "connect";
-}
+export const isConnectRoomMessage = (
+	msg: unknown,
+): msg is connectRoomMessage => {
+	return (
+		typeof msg === "object" &&
+		msg !== null &&
+		"type" in msg &&
+		typeof msg.type === "string" &&
+		msg.type === "connect" &&
+		"roomHash" in msg &&
+		typeof msg.roomHash === "string" &&
+		"clientId" in msg &&
+		typeof msg.clientId === "string" &&
+		"message" in msg &&
+		typeof msg.message === "string" &&
+		(msg.message === "create" || msg.message === "join")
+	);
+};
 
-export const isUpdateFireMessage = (msg: any): msg is UpdateFireMessage => {
-  return msg.type === "updateFire";
-}
+export const isUpdateFireMessage = (msg: unknown): msg is UpdateFireMessage => {
+	return (
+		typeof msg === "object" &&
+		msg !== null &&
+		"type" in msg &&
+		typeof msg.type === "string" &&
+		msg.type === "updateFire" &&
+		"roomHash" in msg &&
+		typeof msg.roomHash === "string" &&
+		"clientId" in msg &&
+		typeof msg.clientId === "string" &&
+		"value" in msg &&
+		typeof msg.value === "number"
+	);
+};
 
-export const isSystemMessage = (msg: any): msg is SystemMessage => {
-  return msg.type === "system";
-}
+export const isSystemMessage = (msg: unknown): msg is SystemMessage => {
+	return (
+		typeof msg === "object" &&
+		msg !== null &&
+		"type" in msg &&
+		typeof msg.type === "string" &&
+		msg.type === "system" &&
+		"roomHash" in msg &&
+		typeof msg.roomHash === "string" &&
+		"message" in msg &&
+		typeof msg.message === "string" &&
+		(msg.message === "start" || msg.message === "finish")
+	);
+};
 
-export const isResultMessage = (msg: any): msg is ResultMessage => {
-  return msg.type === "result";
-}
-
-export const bindConnectRoomMessage = (msg: any): (connectRoomMessage | null) => {
-  if (isConnectRoomMessage(msg)) {
-    return msg as connectRoomMessage;
-  }
-  return null;
-}
-
-export const bindUpdateFireMessage = (msg: any): (UpdateFireMessage | null) => {
-  if (isUpdateFireMessage(msg)) {
-    return msg as UpdateFireMessage;
-  }
-  return null;
-}
-
-export const bindSystemMessage = (msg: any): (SystemMessage | null) => {
-  if (isSystemMessage(msg)) {
-    return msg as SystemMessage;
-  }
-  return null;
-}
-
-export const bindResultMessage = (msg: any): (ResultMessage | null) => {
-  if (isResultMessage(msg)) {
-    return msg as ResultMessage;
-  }
-  return null;
-}
+export const isResultMessage = (msg: unknown): msg is ResultMessage => {
+	return (
+		typeof msg === "object" &&
+		msg !== null &&
+		"type" in msg &&
+		typeof msg.type === "string" &&
+		msg.type === "result" &&
+		"roomHash" in msg &&
+		typeof msg.roomHash === "string" &&
+		"result" in msg &&
+		Array.isArray(msg.result) &&
+		msg.result.every((r) => r instanceof Map)
+	);
+};
