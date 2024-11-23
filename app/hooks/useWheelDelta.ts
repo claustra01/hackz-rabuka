3c100d7969291b76b18ce17d974a59dd7df333ef
claustra01 <shinya430.g@gmail.com>
2024-11-24 03:02:42 +0900

    add: wheel delta hook

---
+++ app/hooks/useWheelDelta.ts
@@ -0,0 +1,39 @@
+import { useEffect, useState } from "hono/jsx";
+
+interface WheelEventState {
+  deltaX: number;
+  deltaY: number;
+}
+
+export const useMouseWheel = () => {
+  const [wheelState, setWheelState] = useState<WheelEventState>({
+    deltaX: 0,
+    deltaY: 0,
+  });
+
+  const resetDelta = () => {
+    setWheelState({
+      deltaX: 0,
+      deltaY: 0,
+    });
+  }
+
+  useEffect(() => {
+    const handleWheel = (event: WheelEvent) => {
+      setWheelState(
+        (prevState) => ({
+          deltaX: prevState.deltaX + Math.abs(event.deltaX),
+          deltaY: prevState.deltaY + Math.abs(event.deltaY),
+        })
+      );
+    };
+
+    window.addEventListener("wheel", handleWheel);
+
+    return () => {
+      window.removeEventListener("wheel", handleWheel);
+    };
+  }, []);
+
+  return { wheelState, resetDelta };
+};
