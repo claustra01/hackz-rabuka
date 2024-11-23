1d08a601f4146d3e158c0c85073844f0871a2c6e
claustra01 <shinya430.g@gmail.com>
2024-11-24 05:42:18 +0900

    update: get room and client id from localstorage

--- app/islands/GameDisplay.tsx
+++ app/islands/GameDisplay.tsx
@@ -1,41 +1,48 @@
-import { useRef } from "hono/jsx";
+import { useEffect, useRef, useState } from "hono/jsx";
+import { UpdateFireMessage } from "../../websocket/src/schemas";
 import { useBloadcast } from "../hooks/useBloadcast";
 import AudioClass from "./AudioClass";
 import ThreeCanvas from "./ThreeCanvas";
 
 export default function GameDisplay() {
-	const ownHonoPoint = useRef<number>(0);
-	const { isReady, fireStatus, sendMessage } = useBloadcast();
-	const room = "room1";
-	const client = "client1";
+	const ownHonoPoint = useRef<number>(1);
+	const [clientId, setClientId] = useState<string>("");
+	const [roomId, setRoomId] = useState<string>("");
+	const { isReady, fireStatus, sendMessage, init } = useBloadcast();
 
-	fireStatus.forEach((value, key) => {
-		if (key === room) {
-			value.forEach((value, key) => {
-				if (key === client) {
-					ownHonoPoint.current = value;
-					console.log(ownHonoPoint.current);
-				}
-			});
+	useEffect(() => {
+		// get roomId and clientId from localStorage
+		const rId = localStorage.getItem("roomId");
+		const cId = localStorage.getItem("clientId");
+		if (!rId || !cId) {
+			window.location.href = "/";
 		}
-	});
+		setRoomId(rId || "");
+		setClientId(cId || "");
+		console.log("roomId: ", roomId);
+		// init fire score
+		if (isReady) {
+			init(roomId);
+		}
+	}, [roomId, clientId, isReady]);
+
+	ownHonoPoint.current = fireStatus.get(roomId)?.get(clientId) || 1;
+	console.log(ownHonoPoint.current);
 
 	return (
 		<>
 			<AudioClass
 				honoPoint={ownHonoPoint}
-				client={client}
-				room={room}
+				client={clientId}
+				room={roomId}
 				isReady={isReady}
 				sendMessage={sendMessage}
 			/>
-			{Array.from(fireStatus.entries()).map(([roomKey, roomValue]) => {
-				if (roomKey === room) {
-					return Array.from(roomValue.entries()).map(
-						([clientKey, clientValue]) => {
-							return <ThreeCanvas honoPoint={clientValue} key={clientKey} />;
-						},
-					);
+			{Array.from(fireStatus.entries()).map(([k, v]) => {
+				if (k === roomId) {
+					return Array.from(v.entries()).map(([cliendId, value]) => {
+						return <ThreeCanvas honoPoint={value} key={cliendId} />;
+					});
 				}
 				return null;
 			})}
