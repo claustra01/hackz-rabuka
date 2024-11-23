22f4b51e9903d82ae2b44d966e51d5484ca7fc0e
Semi-koron <6jimoto6@gmail.com>
2024-11-23 20:34:06 +0900

    fix lint error

--- app/islands/threeCanvas.tsx
+++ app/islands/threeCanvas.tsx
@@ -4,29 +4,29 @@ import { useFireCreate } from "../hooks/useFireCreate";
 import { useInitThree } from "../hooks/useInitThree";
 
 export default function ThreeCanvas() {
-  const mountRef = useRef<HTMLDivElement>(null);
-  const rendererRef = useRef<THREE.WebGLRenderer | null>(null);
-  const cameraRef = useRef<THREE.PerspectiveCamera | null>(null);
-  const sceneRef = useRef<THREE.Scene | null>(null);
-  const lightRef = useRef<THREE.DirectionalLight | null>(null);
-  const objectRef = useRef<THREE.Mesh | null>(null);
+	const mountRef = useRef<HTMLDivElement>(null);
+	const rendererRef = useRef<THREE.WebGLRenderer | null>(null);
+	const cameraRef = useRef<THREE.PerspectiveCamera | null>(null);
+	const sceneRef = useRef<THREE.Scene | null>(null);
+	const lightRef = useRef<THREE.DirectionalLight | null>(null);
+	const objectRef = useRef<THREE.Mesh | null>(null);
 
-  useEffect(() => {
-    //mountRefのサイズを設定
-    if (!mountRef.current) return;
-    const width = window.innerWidth;
-    const height = window.innerHeight;
-    mountRef.current.style.width = `${width}px`;
-    mountRef.current.style.height = `${height}px`;
-    useInitThree(mountRef, rendererRef, cameraRef, sceneRef, lightRef);
-  });
+	useEffect(() => {
+		//mountRefのサイズを設定
+		if (!mountRef.current) return;
+		const width = window.innerWidth;
+		const height = window.innerHeight;
+		mountRef.current.style.width = `${width}px`;
+		mountRef.current.style.height = `${height}px`;
+		useInitThree(mountRef, rendererRef, cameraRef, sceneRef, lightRef);
+	});
 
-  useEffect(() => {
-    //cubeを追加
-    if (sceneRef.current) {
-      useFireCreate(sceneRef);
-    }
-  }, [sceneRef.current]);
+	useEffect(() => {
+		//cubeを追加
+		if (sceneRef.current) {
+			useFireCreate(sceneRef);
+		}
+	}, [sceneRef.current]);
 
-  return <div ref={mountRef} />;
+	return <div ref={mountRef} />;
 }
